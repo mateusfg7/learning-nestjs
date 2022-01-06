455ab2ce5ff260ed2effb5934f738bad04b714cd
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-01-06 18:04:44 -0300

    feat: create cats module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  controllers: [CatsController],
-  providers: [CatsService],
+  imports: [CatsModule],
 })
 export class AppModule {}
