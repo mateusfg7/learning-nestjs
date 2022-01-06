8ef42c1988d433cae231ed94f7cf3f0307632bf0
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-01-06 15:08:09 -0300

    refactor: update app module
    Remove `imports` and `providers`

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { CatsController } from './cats/cats.controller';
 
 @Module({
-  imports: [],
   controllers: [CatsController],
-  providers: [],
 })
 export class AppModule {}
