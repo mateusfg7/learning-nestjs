8e9c0e34f576fb081dbe783f64f07467bab56bef
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-01-06 15:05:40 -0300

    feat: create cats controller

---
+++ src/cats/cats.dto.ts
@@ -0,0 +1,15 @@
+export class CreateCatDto {
+  name: string;
+  age: number;
+  bread: string;
+}
+
+export class UpdateCatDto {
+  name: string;
+  age: number;
+  bread: string;
+}
+
+export class ListAllEntities {
+  limit: number;
+}
