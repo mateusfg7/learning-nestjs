3886e6020ae1af852c12c70d2826dfb913757403
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-01-11 14:01:01 -0300

    feat: use validation pipe on \`app.module\`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { logger } from './common/middleware/logger.middleware';
 import { CatsModule } from './cats/cats.module';
+import { APP_PIPE } from '@nestjs/core';
+import { ValidationPipe } from './cats/validation.pipe';
 
 @Module({
   imports: [CatsModule],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
